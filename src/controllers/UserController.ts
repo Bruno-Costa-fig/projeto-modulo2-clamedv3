b3434f4a728ff1b5b37c19d31e22b6a37270f097
bruno_costa_fig <brunocosta.fig@gmail.com>
2025-02-19 20:30:06 -0300

    feat: base do create do usuário

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,15 +1,35 @@
 import { Request, Response } from "express";
 import { AppDataSource } from "../data-source";
+import { ProfileEnum, User } from "../entities/User";
+import { Driver } from "../entities/Driver";
+import { UserCreateRequest } from "../classes/UserCreateRequest";
 
 class UserController {
-
+  private userRepository;
+  private driverRepository;
 
   constructor() {
-    
+    this.userRepository = AppDataSource.getRepository(User)
+    this.driverRepository = AppDataSource.getRepository(Driver)
   }
 
-  create = (req: Request, res: Response) => {
-    
+  create = async (req: Request, res: Response) => {
+    let userBody = req.body as UserCreateRequest
+
+    let user = await this.userRepository.save({
+      name: userBody.name,
+      password: userBody.password, // tem que criptografar
+      profile: userBody.profile
+    })
+
+    if(userBody.profile == ProfileEnum.DRIVER){
+      this.driverRepository.save({
+        user_id: user.id,
+        document: userBody.document
+      })
+    } else if(userBody.profile == ProfileEnum.BRANCH){
+      // lógica para a branch
+    }
   };
 
 
